44f6200514c4f138890552e1a7d279e1e4216237
micalo-mica <nwaforchukwukamicah@gmail.com>
2023-08-12 16:36:49 +0100

    footer data file added

--- fiverr/src/components/Footer.jsx
+++ fiverr/src/components/Footer.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import styled from "styled-components";
+
+const F = styled.div`
+  @media (max-width: ${({ theme }) => theme.screens.lg1}) {
+  }
+  @media (max-width: ${({ theme }) => theme.screens.md}) {
+  }
+  @media (max-width: ${({ theme }) => theme.screens.sm3}) {
+  }
+  @media (max-width: ${({ theme }) => theme.screens.sm2}) {
+  }
+  @media (max-width: ${({ theme }) => theme.screens.sm1}) {
+  }
+`;
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <F>
+      <FContainer>
+        <Top></Top>
+        <Bottom> </Bottom>
+      </FContainer>
+    </F>
+  );
 }
 
 export default Footer;
---
+++ fiverr/src/static/foot.js
@@ -0,0 +1,125 @@
+export const footerProductLinks = [
+  {
+    name: "About us",
+    link: "/about",
+  },
+  {
+    name: "Careers",
+    link: "/carrers",
+  },
+  {
+    name: "Store Locations",
+  },
+  {
+    name: "Our Blog",
+  },
+  {
+    name: "Reviews",
+  },
+];
+
+export const footercompanyLinks = [
+  {
+    name: "Game & Video",
+  },
+  {
+    name: "Phone &Tablets",
+  },
+  {
+    name: "Computers & Laptop",
+  },
+  {
+    name: "Sport Watches",
+  },
+  {
+    name: "Events",
+  },
+];
+
+export const footerSupportLinks = [
+  {
+    name: "FAQ",
+  },
+  {
+    name: "Reviews",
+  },
+  {
+    name: "Contact Us",
+  },
+  {
+    name: "Shipping",
+  },
+  {
+    name: "Live chat",
+  },
+];
+
+// ================================foot=============================
+
+export const footerLinks = [
+  // 1
+  {
+    category: "compony",
+    link: [
+      {
+        name: "About us",
+        link: "/about",
+      },
+      {
+        name: "Careers",
+        link: "/carrers",
+      },
+      {
+        name: "Store Locations",
+      },
+      {
+        name: "Our Blog",
+      },
+      {
+        name: "Reviews",
+      },
+    ],
+  },
+  //   2
+  {
+    category: "compony",
+    link: [
+      {
+        name: "Game & Video",
+      },
+      {
+        name: "Phone &Tablets",
+      },
+      {
+        name: "Computers & Laptop",
+      },
+      {
+        name: "Sport Watches",
+      },
+      {
+        name: "Events",
+      },
+    ],
+  },
+  //   3
+  {
+    category: "compony",
+    link: [
+      {
+        name: "FAQ",
+      },
+      {
+        name: "Reviews",
+      },
+      {
+        name: "Contact Us",
+      },
+      {
+        name: "Shipping",
+      },
+      {
+        name: "Live chat",
+      },
+    ],
+  },
+];
